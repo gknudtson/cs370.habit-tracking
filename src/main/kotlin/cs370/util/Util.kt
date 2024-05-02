c5b54a173f3be45ec9f56d6fa1be8848efacb2a0
gknudtson <gabeknudtson+github@gmail.com>
2024-05-01 23:07:46 -0600

    :)

--- src/main/kotlin/cs370/util/Util.kt
+++ src/main/kotlin/cs370/util/Util.kt
@@ -2,10 +2,12 @@ package cs370.util
 
 import java.time.LocalDate as JavaLocalDate
 import kotlinx.datetime.LocalDate
+import kotlinx.serialization.Serializable
 
 fun JavaLocalDate.toKotlinxLocalDate(): LocalDate {
     return LocalDate(year, monthValue, dayOfMonth)
 }
+@Serializable
 data class HabitDTO(
     val userId: Int,
     val name: String,
